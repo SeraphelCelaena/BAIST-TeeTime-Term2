722d9a665191b49db7aae06ae7d6bf02c7dd7a36
serpag <67750475+SeraphelCelaena@users.noreply.github.com>
2026-03-04 17:16:43 -0700

    TeeTime model

---
+++ TeeTimeWebApp/Models/TeeTime.cs
@@ -0,0 +1,10 @@
+namespace TeeTimeWebApp.Models
+{
+	public class TeeTime
+	{
+		public int TeeTimeID { get; set; }
+		public DateOnly Date { get; set; }
+		public TimeOnly StartTime { get; set; }
+		public bool Confirmed { get; set; }
+	}
+}
